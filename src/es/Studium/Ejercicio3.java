c4427c15c25ee1e4d5807e629bf58dbf38ee5b75
AlvCarballo <alv.carballo@outlook.es>
2020-01-17 08:58:34 +0100

    fist

---
+++ src/es/Studium/Ejercicio3.java
@@ -0,0 +1,14 @@
+package es.Studium;
+public class Ejercicio3 
+{
+	public static void main(String[] args) 
+	{
+		String mensaje = "El bucle se ha ejecutado: ";
+		int contador = 1;
+		while (contador < 100) 
+		{
+			System.out.println(mensaje + contador + " veces");
+			contador++;
+		}
+	} 
+}
